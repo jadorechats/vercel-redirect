9346f6632013644a32e0a9abf76d6242e97b6320
jadorechats <136388149+jadorechats@users.noreply.github.com>
2023-06-13 00:21:05 -1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+      },
+      {
+        source: "/index",
+        destination: "/_index",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
